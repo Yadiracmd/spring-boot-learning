da6bc1a01760a60f2c834ad96314e43b01c9e3d1
xxiaoccpm <3570544406@qq.com>
2024-04-01 20:31:32 +0800

    4.1 springboot使用日志功能

---
+++ spring-boot-log/src/main/java/com/ztf/boot/log/LogApplication.java
@@ -0,0 +1,13 @@
+package com.ztf.boot.log;
+
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+}
